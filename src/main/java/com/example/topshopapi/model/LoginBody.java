624d1283b326c95053a75df946a3af3c3e7ea506
Vicente Figueroa <figueroa.vicente9@gmail.com>
2023-01-27 02:36:28 -0800

    added validation annotations to login body object

--- src/main/java/com/example/topshopapi/model/LoginBody.java
+++ src/main/java/com/example/topshopapi/model/LoginBody.java
@@ -1,8 +1,16 @@
 package com.example.topshopapi.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class LoginBody {
 
+    @NotNull
+    @NotBlank
     private String username;
+
+    @NotNull
+    @NotBlank
     private String password;
 
     public String getUsername() {
